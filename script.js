f748c4ef4b697f66ce813f2715d1bedf93cfd146
NicoSciangula <nico.sciangula99@gmail.com>
2026-01-14 20:46:35 +0100

    added function to create members card and added a cycle for to read the array with obj

--- script.js
+++ script.js
@@ -3,36 +3,56 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "img/male1.png",
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "img/female1.png",
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "img/male2.png",
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "img/female2.png",
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "img/male3.png",
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
-  }
+    img: "img/female3.png",
+  },
 ];
+
+function generateCard(member) {
+  const { name, role, email, img } = member;
+  const newDiv = document.createElement("div");
+  newDiv.innerHTML = `<div class="card">
+                <img src="./${img}" alt="picture of the team-member">
+                <div class="card-description">
+                    <h2>${name}</h2>
+                    <h3>${role}</h3>
+                    <p>${email}</p>
+                </div>
+            </div>`;
+  return newDiv;
+}
+
+const containerCard = document.querySelector(".container");
+
+for (let i = 0; i < teamMembers.length; i++) {
+  containerCard.append(generateCard(teamMembers[i]));
+}
